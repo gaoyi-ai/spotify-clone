8879828f3185ca0ce735270ea5ef7b97062d5072
gaoyi <577981827@qq.com>
2021-12-13 16:20:57 +0800

    feat: project init

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { getSession } from "next-auth/react";
+import Head from "next/head";
+import Centre from "../components/Centre";
+import Player from "../components/Player";
+import SideBar from "../components/SideBar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="flex">
+        {/* sidebar */}
+        <SideBar />
+
+        {/* centre */}
+        <Centre />
+      </main>
+      <div className="sticky bottom-0">
+        {/* player */}
+        <Player />
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
